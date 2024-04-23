9da235c41301a0d29b2d5391624d5ba4627692e0
DDorka02 <dobszaydorka@gmail.com>
2024-04-23 09:00:33 +0200

    kitalalos

---
+++ src/main/CsigaversenyJatek.java
@@ -0,0 +1,19 @@
+package main;
+
+
+public class CsigaversenyJatek  extends Jatek{
+    
+    
+    public CsigaversenyJatek(){
+        
+    }
+    
+    
+    @Override
+    void start(){
+        super.start();
+//        System.out.println("Elindult!");
+}
+}
+
+   
